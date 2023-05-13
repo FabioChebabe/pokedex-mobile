775f871c6af70ae8665228f98ea1f7eac62188a0
Fabio Chebabe Carnelli <fabio.carnelli@atomicbuild.com>
2023-05-12 22:33:17 -0300

    initial setup

--- App.js
+++ App.js
@@ -1,10 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
+import { useCallback, useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import usePokemonApi from './src/hooks/usePokemonApi';
 
 export default function App() {
+  const { api } = usePokemonApi();
+  const [isLoading, setIsLoading] = useState(true);
+  const [pokemons, setPokemons] = useState([]);
+
+  const getPokemonsList = useCallback(
+    async (offset = 0) => {
+      try {
+        setIsLoading(true);
+        const response = await api.listPokemons(offset);
+
+        response.results.map((pokemon) => {
+          setPokemons((prevState) => [...prevState, pokemon]);
+        });
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setIsLoading(false);
+      }
+    },
+    [api]
+  );
+
+  useEffect(() => {
+    getPokemonsList();
+  }, [getPokemonsList]);
+
+  console.log(pokemons);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Pokedex</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/hooks/usePokemonApi.ts
@@ -0,0 +1,10 @@
+import { PokemonClient } from 'pokenode-ts';
+import { useMemo } from 'react';
+
+const usePokemonApi = () => {
+    const api = useMemo(() => new PokemonClient(), []);
+
+    return { api };
+};
+
+export default usePokemonApi;
