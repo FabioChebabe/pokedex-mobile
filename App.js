11c7c0172c5f858b9fb2ad486c064a52f579903a
Fabio Chebabe Carnelli <fabioccarnelli@gmail.com>
2023-05-16 23:49:48 -0300

    create theme

--- App.js
+++ App.js
@@ -6,17 +6,21 @@ import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './src/pages/Home';
 import DetailsScreen from './src/pages/Details';
+import { ThemeProvider } from 'styled-components/native';
+import { theme } from './src/theme';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <ThemeProvider theme={theme}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Details" component={DetailsScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ThemeProvider>
   );
 }
 
--- src/pages/Details/index.tsx
+++ src/pages/Details/index.tsx
@@ -1,11 +1,15 @@
 import { useRoute } from '@react-navigation/native';
 import { Image, Text, View } from 'react-native';
+import { useTheme } from 'styled-components/native';
 
 function DetailsScreen() {
   const { params } = useRoute();
+  const theme = useTheme()
+  const pokemonType = params.pokemon.types[0].type.name
+
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: theme.colors.pokemonType[pokemonType] }}>
       <Text>Details Screen</Text>
       <Text>{params.pokemon.name}</Text>
       <Text>{params.pokemon.id}</Text>
