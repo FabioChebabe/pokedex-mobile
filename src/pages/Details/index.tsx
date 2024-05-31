840235f14e58784ccb238977c918633d64b237d7
Fabio Chebabe Carnelli <fabioccarnelli@gmail.com>
2024-05-31 19:40:36 -0300

    feat: details ui tweaks

--- src/pages/Details/index.tsx
+++ src/pages/Details/index.tsx
@@ -1,5 +1,5 @@
 import { useNavigation, useRoute } from '@react-navigation/native';
-import { Button, Image, Text, TouchableOpacity, View } from 'react-native';
+import { Dimensions, Image, Text, TouchableOpacity, View } from 'react-native';
 import { useTheme } from 'styled-components/native';
 import Tag from '../../components/Tags';
 import { SafeAreaView } from 'react-native-safe-area-context';
@@ -7,6 +7,7 @@ import usePokemonApi from '../../hooks/usePokemonApi';
 import { useState } from 'react';
 import { AntDesign } from '@expo/vector-icons';
 import DetailsLoading from './Loading';
+import TextWrapped from '../../components/Text';
 
 function DetailsScreen() {
   const { params } = useRoute();
@@ -16,6 +17,16 @@ function DetailsScreen() {
   const pokemonType = pokemon.types[0].type.name;
   const navigation = useNavigation();
   const { api } = usePokemonApi();
+  const { width } = Dimensions.get('window');
+  const aboutBoxWidth = (width - 50) / 3;
+  const statusAbreviation = {
+    hp: 'hp',
+    attack: 'atk',
+    defense: 'def',
+    'special-attack': 'satk',
+    'special-defense': 'sdef',
+    speed: 'spd',
+  };
 
   const getNextPokemon = async () => {
     setLoading(true);
@@ -102,52 +113,108 @@ function DetailsScreen() {
                 alignSelf: 'center',
               }}
             />
-            <View style={{ flexDirection: 'row', gap: 16 }}>
+            <View style={{ flexDirection: 'row', gap: 16, paddingBottom: 16 }}>
               {pokemon.types.map((type, idx) => (
                 <Tag type={type.type.name} key={`${idx}-${type.type.name}`} />
               ))}
             </View>
-            <Text
-              style={{
-                color: theme.colors.pokemonType[pokemonType],
-                fontSize: 14,
-                fontWeight: '700',
-              }}
+            <TextWrapped
+              typography="subtitle1"
+              color={theme.colors.pokemonType[pokemonType]}
             >
               About
-            </Text>
+            </TextWrapped>
 
             <View
               style={{
                 flexDirection: 'row',
                 paddingVertical: 24,
                 alignItems: 'center',
+                justifyContent: 'center',
               }}
             >
-              <View style={{ alignItems: 'center', paddingHorizontal: 16 }}>
-                <Text>{pokemon.weight}</Text>
-                <Text>Weight</Text>
+              <View
+                style={{
+                  alignItems: 'center',
+                  paddingHorizontal: 16,
+                  width: aboutBoxWidth,
+                  gap: 16,
+                }}
+              >
+                <TextWrapped
+                  typography="body3"
+                  color={theme.colors.grayScale.dark}
+                >
+                  {pokemon.weight}
+                </TextWrapped>
+                <TextWrapped
+                  typography="caption"
+                  color={theme.colors.grayScale.medium}
+                >
+                  Weight
+                </TextWrapped>
               </View>
-              <View style={{ alignItems: 'center', paddingHorizontal: 16 }}>
-                <Text>{pokemon.height}</Text>
-                <Text>Height</Text>
+              <View
+                style={{ width: 1, height: '100%', backgroundColor: 'gray' }}
+              />
+              <View
+                style={{
+                  alignItems: 'center',
+                  paddingHorizontal: 16,
+                  width: aboutBoxWidth,
+                  gap: 16,
+                }}
+              >
+                <TextWrapped
+                  typography="body3"
+                  color={theme.colors.grayScale.dark}
+                >
+                  {pokemon.height}
+                </TextWrapped>
+                <TextWrapped
+                  typography="caption"
+                  color={theme.colors.grayScale.medium}
+                >
+                  Height
+                </TextWrapped>
               </View>
-              <View style={{ alignItems: 'center', paddingHorizontal: 16 }}>
-                {pokemon?.abilities?.map((ability) => (
-                  <Text key={ability.ability.name}>{ability.ability.name}</Text>
-                ))}
-                <Text>Moves</Text>
+              <View
+                style={{ width: 1, height: '100%', backgroundColor: 'gray' }}
+              />
+              <View
+                style={{
+                  alignItems: 'center',
+                  paddingHorizontal: 16,
+                  width: aboutBoxWidth,
+                  gap: 16,
+                }}
+              >
+                <View style={{ gap: 4 }}>
+                  {pokemon?.abilities?.map((ability) => (
+                    <TextWrapped
+                      typography="body3"
+                      color={theme.colors.grayScale.dark}
+                      key={ability.ability.name}
+                    >
+                      {ability.ability.name}
+                    </TextWrapped>
+                  ))}
+                </View>
+                <TextWrapped
+                  typography="caption"
+                  color={theme.colors.grayScale.medium}
+                >
+                  Moves
+                </TextWrapped>
               </View>
             </View>
-            <Text
-              style={{
-                color: theme.colors.pokemonType[pokemonType],
-                fontSize: 14,
-                fontWeight: '700',
-              }}
+            <TextWrapped
+              typography="subtitle1"
+              color={theme.colors.pokemonType[pokemonType]}
+              style={{ marginBottom: 16 }}
             >
               Base stats
-            </Text>
+            </TextWrapped>
             {pokemon.stats.map((stat) => (
               <View
                 style={{
@@ -158,12 +225,42 @@ function DetailsScreen() {
                   paddingHorizontal: 8,
                 }}
               >
-                <Text>
-                  {stat.stat.name} {'->'} {stat.base_stat}
-                </Text>
                 <View
                   style={{
-                    width: 200,
+                    width: '30%',
+                    flexDirection: 'row',
+                    justifyContent: 'flex-end',
+                  }}
+                >
+                  <TextWrapped
+                    typography="subtitle3"
+                    color={theme.colors.pokemonType[pokemonType]}
+                    style={{ paddingHorizontal: 12 }}
+                  >
+                    {statusAbreviation[stat.stat.name].toUpperCase()}{' '}
+                  </TextWrapped>
+                  <View
+                    style={{
+                      height: 10,
+                      width: 1,
+                      backgroundColor: theme.colors.grayScale.light,
+                    }}
+                  />
+                  <TextWrapped
+                    typography="body3"
+                    color={theme.colors.grayScale.dark}
+                    style={{
+                      paddingHorizontal: 12,
+                      minWidth: 45,
+                    }}
+                    align="left"
+                  >
+                    {`${('00' + stat.base_stat).slice(-3)}`}
+                  </TextWrapped>
+                </View>
+                <View
+                  style={{
+                    width: '70%',
                     height: 8,
                     backgroundColor: `${theme.colors.pokemonType[pokemonType]}33`,
                   }}
